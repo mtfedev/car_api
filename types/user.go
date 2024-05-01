d284c7fe717f2eda1e81ef3ac22e9925410f7b88
Mateusz Fedorowicz <fedorowiczmati@outlook.com>
2024-05-02 00:00:04 +0200

    user v0.6

--- types/user.go
+++ types/user.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 
 	"golang.org/x/crypto/bcrypt"
@@ -13,6 +15,7 @@ const (
 	minUserSurname = 2
 	maxUserSurname = 64
 	minPasswodLen  = 7
+	minEmail       = 3
 )
 
 type User struct {
@@ -63,4 +66,24 @@ func (p *CreateUserParams) ValidateName() bool {
 	if len(p.Surname) < minUserSurname || len(p.Surname) > maxUserSurname {
 		return false
 	}
+
+	return false
+}
+func (params CreateUserParams) Validate() map[string]string {
+	errors := map[string]string{}
+	if len(params.Password) < minPasswodLen {
+		errors["password"] = fmt.Sprintf("password length should be at lesast %d characters", minPasswodLen)
+	}
+	if !isEmailValid(params.Email) {
+		errors["email"] = fmt.Sprintf("email is invalid %d", minEmail)
+	}
+	return errors
+}
+
+func IsValidPassword(encpw, pw string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(encpw), []byte(pw)) == nil
+}
+func isEmailValid(e string) bool {
+	emailRegex := regexp.MustCompile(`a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(e)
 }
